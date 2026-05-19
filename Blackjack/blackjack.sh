8fe78d27e4d0c49a99e64481e4dc84372ae49207
Scott <sjtlive@hotmail.com>
2026-05-19 19:15:58 +0100

    Deal functino now: stores number of cards in deck + makes random number for position in deck + stores card at that position + adds it to player

--- Blackjack/blackjack.sh
+++ Blackjack/blackjack.sh
@@ -2,12 +2,17 @@
 
 #This array contains every card inside the deck, it will get smaller as the game progresses and be reset as a new game begins
 
-cards=("AH" "2H" "3H" "4H" "5H" "6H" "7H" "8H" "9H" "10H" "JH" "QH" "KH" "AD" "2D" "3D" "4D" "5D" "6D" "7D" "8D" "9D" "10D" "JD" "QD" "KD" "AS" "2S" "3S" "4S" "5S" "6S" "7S" "8S" "9S" "10S" "JS" "QS" "KS" "AC" "2C" "3C" "4C" "5C" "6C" "7C" "8C" "9C" "10C" "JC" "QC" "KC")
-
+deck=("AH" "2H" "3H" "4H" "5H" "6H" "7H" "8H" "9H" "10H" "JH" "QH" "KH" "AD" "2D" "3D" "4D" "5D" "6D" "7D" "8D" "9D" "10D" "JD" "QD" "KD" "AS" "2S" "3S" "4S" "5S" "6S" "7S" "8S" "9S" "10S" "JS" "QS" "KS" "AC" "2C" "3C" "4C" "5C" "6C" "7C" "8C" "9C" "10C" "JC" "QC" "KC")
 
 #Player and dealer card arrays, contains the cards the player and dealer have been dealt
-player[]
-dealer[]
+#https://stackoverflow.com/a/53018127
+
+# Source - https://stackoverflow.com/a/53018127
+# Posted by tmt, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-05-19, License - CC BY-SA 4.0
+
+declare -a player=()
+declare -a dealer=()
 
 
 
@@ -24,13 +29,25 @@ random() {
     echo $number
 
 }
-random 52
+#random 52
 ############################################################
 
 deal() {
 
+    #num_cards stores the current number of cards in the deck array
+    num_cards="${#deck[@]}"
 
+    #number variable stores a randomly generated number that ranges from position 0 to N-1, where N=number of cards in deck
+    number=$(( $RANDOM % $num_cards ))
 
+    #This stores an item from the array (at index position determined in previous line), in a variable
+    card=${deck[number]}
+    echo "This is the card to be dealt "$card
 
+    #Now the card will be added to the array for either player cards or dealer cards
+
+    player+=($card)
 
 }
+
+deal
